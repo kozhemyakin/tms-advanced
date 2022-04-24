60c5fd9d557a9db5f9bb479f04a8158716cb476f
kozhemyakin <pctdoc@gmail.com>
2022-04-24 16:59:08 +0400

    solve Algorithms. Hours to seconds task

---
+++ hw_3.js
@@ -0,0 +1,9 @@
+// Написать функцию, которая принимает количество часов, переводит часы в секунды и возвращает полученное значение.
+// Примеры:
+// hoursToSeconds(10) --> 36000
+// hoursToSeconds(2) --> 7200
+// hoursToSeconds(1,5) --> 5400
+
+const hoursToSeconds = (hours) => hours * 60 * 60
+
+hoursToSeconds(1)
