252aaa75395402e517139609c48c97cdeac03372
kozhemyakin <pctdoc@gmail.com>
2022-04-28 18:43:55 +0400

    solve 9_state_game_for_cats task

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Checkbox from './Checkbox';
 import './App.css';
+import Cats from './Cats';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Counter /> */}
 
-    <Checkbox />
+    {/* <Checkbox /> */}
+    <Cats />
     
   </React.StrictMode>,
   document.getElementById('root')
