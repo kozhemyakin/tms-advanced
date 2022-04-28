668699d40a1d5b0e7e3e561652e422e3a7e8f1bf
kozhemyakin <pctdoc@gmail.com>
2022-04-28 17:23:56 +0400

    solve 8_state_cursive_text task

---
+++ my-app/src/ItalicText.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactTooltip from 'react-tooltip';
+
+function ItalicText({ italic }) {
+    const [ state, setState ] = React.useState(false);
+
+    const changeState = () => {
+        setState(!state)
+    }
+
+    let style = {
+        'font-style': '',
+    };
+
+    let className = '';
+    let btn = 'btn';
+
+    if (state === true) {
+        style = {
+            'font-style': 'italic',
+        }
+
+        className='darkBtn';
+    }
+
+    return (
+    <div>
+        <span style={style}>Italic text is here </span>
+        <button onClick={changeState} className={`${className} ${btn}`} data-tip="hello world">I</button>
+        <ReactTooltip place="top" type="dark" effect="float"/>
+    </div>
+    
+    )
+}
+
+export default ItalicText;
