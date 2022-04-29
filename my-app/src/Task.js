4b22b908e26b926379db916053fc474aae09aaeb
kozhemyakin <pctdoc@gmail.com>
2022-04-29 09:47:55 +0400

    init

---
+++ my-app/src/Task.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Task() {
+
+    return (
+    <div>
+        <button onClick={} style={style} ></button>
+    </div>
+    )
+}
+
+export default Task;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import './App.css';
 import Cats from './Cats';
 import ItalicText from './ItalicText'
+import Task from './Task'
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Counter /> */}
     {/* <Checkbox /> */}
-    <Cats />
-    <ItalicText />
+    {/* <Cats />
+    <ItalicText /> */}
+    <Task />
     
   </React.StrictMode>,
   document.getElementById('root')
