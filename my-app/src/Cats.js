822c18a0a864d5194baacc645756834da1b60e7a
kozhemyakin <pctdoc@gmail.com>
2022-04-29 08:43:13 +0300

    solve 9_state_game_for_cats task (#5)
    * solve 9_state_game_for_cats task
    
    * fix critical borders

---
+++ my-app/src/Cats.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+function Cats() {
+    const [ positionX, setPositionX ] = React.useState(0);
+    const [ positionY, setPositionY ] = React.useState(0);
+    const circle = 'circle';
+
+    const changeirclePositionX = () => {
+        setPositionX(Math.random() * 900);
+        setPositionY(Math.random() * 900);
+    }
+
+    const style = {
+        left: positionX + 'px',
+        top: positionY + 'px',
+    }
+
+    return (
+    <div className="field">
+        <button onClick={changeirclePositionX} className={circle} style={style} ></button>
+    </div>
+    )
+}
+
+export default Cats;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './App.css';
+import Cats from './Cats';
 import ItalicText from './ItalicText'
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Counter /> */}
-
     {/* <Checkbox /> */}
-
+    <Cats />
     <ItalicText />
     
   </React.StrictMode>,
