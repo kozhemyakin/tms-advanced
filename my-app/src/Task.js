c3372dc57f05b1ceaf3e2195e3e80919db022030
kozhemyakin <pctdoc@gmail.com>
2022-04-29 11:48:04 +0400

    resolve 16_props_task_list task

--- my-app/src/Task.js
+++ my-app/src/Task.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-function Task() {
+function Task({ number, text, done}) {
+
+    const styles = done ? {textDecoration: 'line-through'} : {textDecoration: 'none'};
 
     return (
-    <div>
-        <button onClick={} style={style} ></button>
+    <div className='taskCheckboxes'>
+        <input type="checkbox" checked={done}></input>
+        <span style={styles}>{number}.&nbsp;</span>
+        <p style={styles}>{text}</p>
     </div>
     )
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -10,10 +10,14 @@ ReactDOM.render(
   <React.StrictMode>
     {/* <Counter /> */}
     {/* <Checkbox /> */}
-    {/* <Cats />
-    <ItalicText /> */}
-    <Task />
-    
+    {/* <Cats /> */}
+    {/* <ItalicText /> */}
+    <Task number={1} text={'first'} done={true} />
+    <Task number={2} text={'second'} done={false} />
+    <Task number={3} text={'third'} done={false} />
+    <Task number={4} text={'fourth'} done={false} />
+    <Task number={5} text={'fifth'} done={false} />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
