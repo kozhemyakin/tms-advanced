0e08deb131748d0076c9e6708ca0d608599080f2
kozhemyakin <pctdoc@gmail.com>
2022-04-29 08:41:25 +0300

    solve 8_state_cursive_text task (#4)
    * solve 8_state_cursive_text task
    
    * fix tooltip text

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Checkbox from './Checkbox';
 import './App.css';
+import ItalicText from './ItalicText'
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Counter /> */}
 
-    <Checkbox />
+    {/* <Checkbox /> */}
+
+    <ItalicText />
     
   </React.StrictMode>,
   document.getElementById('root')
