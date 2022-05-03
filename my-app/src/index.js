ba0078bfcb3c50e6bf2cdd3ee9f88cd76da13f9a
kozhemyakin <pctdoc@gmail.com>
2022-05-03 11:38:50 +0300

    16_props_task_list (checkboxes) (#6)
    * init
    
    * resolve 16_props_task_list task

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import './App.css';
 import Cats from './Cats';
 import ItalicText from './ItalicText'
+import Task from './Task'
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Counter /> */}
     {/* <Checkbox /> */}
-    <Cats />
-    <ItalicText />
-    
+    {/* <Cats /> */}
+    {/* <ItalicText /> */}
+    <Task number={1} text={'first'} done={true} />
+    <Task number={2} text={'second'} done={false} />
+    <Task number={3} text={'third'} done={false} />
+    <Task number={4} text={'fourth'} done={false} />
+    <Task number={5} text={'fifth'} done={false} />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
