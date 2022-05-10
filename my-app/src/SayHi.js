f6c3451c0dc257dcc679ce25602035ff356715ec
kozhemyakin <pctdoc@gmail.com>
2022-05-10 19:01:14 +0400

    fix return

--- my-app/src/SayHi.js
+++ my-app/src/SayHi.js
@@ -11,14 +11,14 @@ function SayHi() {
             setLoading(false);
 
             setGreeting('Hello !!!');
-        }, 2000);
+        }, 3000);
     }
 
     return (
     <div>
         <button onClick={greet} className="btn">SAY HI!</button>
         {loading && <div className="loader" ></div>}
-        {greeting === 'Hello !!!' && !loading && <span>{greeting}</span>}
+        {!loading && <span>{greeting}</span>}
     </div>
     )
 }
