f10883bdc237f7f96acb7712b6576886750d4349
kozhemyakin <pctdoc@gmail.com>
2022-05-16 15:54:07 +0400

    24. useEffect training (#13)
    * solve task
    
    * add zero before number
    
    Co-authored-by: Serge <serge@Serges-MacBook-Pro.local>

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,6 +6,7 @@ import Cats from './Cats';
 import ItalicText from './ItalicText'
 import Task from './Task'
 import SayHi from './SayHi'
+import TimePicker from './TimePicker'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,7 +19,8 @@ ReactDOM.render(
     <Task number={3} text={'third'} done={false} />
     <Task number={4} text={'fourth'} done={false} />
     <Task number={5} text={'fifth'} done={false} /> */}
-    <SayHi />
+    {/* <SayHi /> */}
+    <TimePicker />
 
   </React.StrictMode>,
   document.getElementById('root')
