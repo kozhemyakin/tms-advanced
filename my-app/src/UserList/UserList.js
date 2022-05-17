c54b65165767061cc3308c1eea0f2a3c07655486
Serge <serge@Serges-MacBook-Pro.local>
2022-05-18 00:29:53 +0400

    add map, filter, reduce methods

--- my-app/src/UserList/UserList.js
+++ my-app/src/UserList/UserList.js
@@ -2,38 +2,49 @@ import data from "./data.json";
 
 function UserList() {
 
-  const mydata_map = data.map( (item) =>  {
+  // MAP
+  const mydata_map = data.map( (item) => {
     return (
         <div key={item._id} value={item._id}>
             <h3>{item.name.first}</h3>
             <h4>{item.name.last}</h4>
+            <h5>{item.age}</h5>
         </div>
     );
   });
 
-  const mydata_filter = filter.map( (item) =>  {
+  // FILTER
+  function aboveEighteeen (item) {
+    return item.age === 40;
+  }
+
+  const mydata_filter = data.filter(aboveEighteeen).map( (item) => {
     return (
         <div key={item._id} value={item._id}>
             <h3>{item.name.first}</h3>
             <h4>{item.name.last}</h4>
+            <h5>{item.age}</h5>
         </div>
     );
-  });
+  });;
 
-  const mydata_reduce = data.map( (item) =>  {
-    return (
+  // REDUCE
+  const mydata_reduce = data.reduce( (acc, item) => {
+      acc.push((
         <div key={item._id} value={item._id}>
-            <h3>{item.name.first}</h3>
-            <h4>{item.name.last}</h4>
+            <h5>{item.age} is here</h5>
         </div>
-    );
-  });
+    ))
 
+    return acc
+  }, []);
 
   return (
-    <container>
-      <div>{mydata_map}</div>
-    </container>
+    <div>
+        <div>{mydata_map}</div>
+        <div>{mydata_filter}</div>
+        <div>{mydata_reduce}</div>
+    </div>
     
 )}
 
